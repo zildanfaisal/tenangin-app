01c3907f1f75ec585f359eb377325ab9d1a683c6
RegiMuhammar <regimuhammar@gmail.com>
2025-10-04 02:12:29 +0700

    ubah dikit

--- apps/core/resources/views/welcome.blade.php
+++ apps/core/resources/views/welcome.blade.php
@@ -49,6 +49,7 @@
                 </nav>
             @endif
         </header>
+        <h1>Regi</h1>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
